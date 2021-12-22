b197ab4c4a64ee2982ca2ebb1fe7a92520d9f1cf
Tymur <stud.tim@gmail.com>
2021-12-22 21:53:20 +0200

    playing with models in HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home.index');
+        $products = Product::all();
+//        $products = Product::where('id', 5)->first();
+//        dd($products);
+        foreach ($products as $product) {
+            echo 'Title: '.$product->title;
+            echo "<br>";
+            echo 'Price: '.$product->price;
+            echo "<br>";
+            echo "-------------------------";
+            echo "<br>";
+        }
+//        return view('home.index');
     }
 }

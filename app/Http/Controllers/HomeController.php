a347f2b1d5fd28ac4b6e188ac95f2df94de8dfef
Tymur <stud.tim@gmail.com>
2021-12-27 18:15:35 +0200

    added product display from database

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,17 +8,21 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        $products = Product::all();
+//        $products = Product::all();
 //        $products = Product::where('id', 5)->first();
 //        dd($products);
-        foreach ($products as $product) {
-            echo 'Title: '.$product->title;
-            echo "<br>";
-            echo 'Price: '.$product->price;
-            echo "<br>";
-            echo "-------------------------";
-            echo "<br>";
-        }
-//        return view('home.index');
+//        foreach ($products as $product) {
+//            echo 'Title: '.$product->title;
+//            echo "<br>";
+//            echo 'Price: '.$product->price;
+//            echo "<br>";
+//            echo "-------------------------";
+//            echo "<br>";
+//        }
+        $products = Product::orderBy('created_at')->take(8)->get();
+
+        return view('home.index', [
+            'products' => @$products
+        ]);
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,5 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    use HasFactory;
+    //use HasFactory;
+    public function images(){
+        return $this->hasMany('App\Models\ProductImage');
+    }
 }

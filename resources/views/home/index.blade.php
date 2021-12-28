21959ff08e3fdde268af506514e226d98e7df5ed
Tymur <stud.tim@gmail.com>
2021-12-28 17:37:48 +0200

    created product card

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -143,10 +143,10 @@
                             //dd($product->images);
                             @endphp
                             <div class="product">
-                                <div class="product_image"><a href="product.html"><img src="images/{{$image}}" alt="{{$product->title}}"></div>
+                                <div class="product_image"><a href="product.html"><img src="/images/{{$image}}" alt="{{$product->title}}"></div>
                                 <div class="product_extra product_new"><a href="categories.html">New</a></div>
                                 <div class="product_content">
-                                    <div class="product_title"><a href="product.html">{{$product->title}}</a></div>
+                                    <div class="product_title"><a href="{{route('showProduct', ['category',$product->id])}}">{{$product->title}}</a></div>
                                     <div class="product_price">${{$product->price}}</div>
                                 </div>
                             </div>
